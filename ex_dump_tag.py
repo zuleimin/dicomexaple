b83a17088132f63a5af1439091fe503a5471e5b4
zuleimin <zuleimin@qq.com>
2018-08-01 12:52:58 +0800

    dump tag

---
+++ ex_dump_tag.py
@@ -0,0 +1,11 @@
+import pylab
+import pydicom
+filename = 'Brain2.dcm'
+ds = pydicom.dcmread(filename)
+ds.dir()  # 查看病人所有信息字典keys
+# print(ds.PatientName)  # 查看病人名字
+print(ds) # 查看病人所有信息字典， 如果出现某key对应值编码错误，先暂时跳过该key
+
+
+
+

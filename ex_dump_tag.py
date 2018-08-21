87296c6463cb1c776d88cc0a40907d1f48e24f1f
zuleimin <zuleimin@qq.com>
2018-08-21 20:55:04 +0800

    ss

--- ex_dump_tag.py
+++ ex_dump_tag.py
@@ -6,11 +6,3 @@ ds.dir()  # 查看病人所有信息字典keys
 # print(ds.PatientName)  # 查看病人名字
 print(ds) # 查看病人所有信息字典， 如果出现某key对应值编码错误，先暂时跳过该key
 # how to make a fast program
-
-
-
-#test again
-
-
-#test again and again
-
